117d72d286ab7b92cfc53ca45c65650bd8404916
Isaac <ishimweisaac123@gmail.com>
2026-02-19 23:20:52 +0200

    ft: worked on feature adding tasks to the list

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("Isaac's Todo App: JavaScript is connected!");
+const inputField = document.getElementById('todo-input');
+const addButton = document.getElementById('add-btn');
+const todoList = document.getElementById('todo-list');
+
+addButton.addEventListener('click', function() {
+    const newTask = document.createElement('li'); 
+    newTask.textContent = inputField.value;
+    todoList.appendChild(newTask);
+    inputField.value = '';
+});
