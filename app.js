1cf0a244c36a906ca5d641ff5131385b2b3790dc
Isaac <ishimweisaac123@gmail.com>
2026-02-20 08:47:45 +0200

    ft/add delete button funtionality and styled it

--- app.js
+++ app.js
@@ -3,8 +3,24 @@ const addButton = document.getElementById('add-btn');
 const todoList = document.getElementById('todo-list');
 
 addButton.addEventListener('click', function() {
-    const newTask = document.createElement('li'); 
-    newTask.textContent = inputField.value;
-    todoList.appendChild(newTask);
-    inputField.value = '';
+    if (inputField.value.trim() !== "") {
+        const newTask = document.createElement('li'); 
+        newTask.textContent = inputField.value;
+
+        // This is the delete button for each task
+        const deleteBtn = document.createElement('button');
+        deleteBtn.textContent = 'x';
+        deleteBtn.className = 'delete-btn';
+
+        //this is the event listener for the delete button
+        deleteBtn.addEventListener('click', function() {
+            newTask.remove();
+        });
+
+        // every task we create will have a delete button
+        newTask.appendChild(deleteBtn);
+        todoList.appendChild(newTask);
+
+        inputField.value = '';
+    }
 });
